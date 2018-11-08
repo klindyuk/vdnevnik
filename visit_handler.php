965a53a11e15c9c4007fa0a52fa72aa78734c322
klindyuk <klindyuk@gmail.com>
2018-11-08 14:43:09 +0300

    Добавлены сообщения для пользователя

--- visit_handler.php
+++ visit_handler.php
@@ -3,18 +3,18 @@
         if ($_REQUEST['action'] == 'checkin') {
             include 'connection.php';
             $sql = "SELECT * FROM users WHERE school_title='".$_REQUEST['schoolTitle']."' AND report_grade='".$_REQUEST['reportGrade']."'";
-            $res = mysqli_query($connect,$sql) or die('Error');
+            $res = mysqli_query($connect,$sql) or die('Ошибка подключения к базе данных');
             if (mysqli_num_rows($res) > 0) {
                 $data = mysqli_fetch_assoc($res);
                 $id = $data['user_id'];
                 $visits = $data['visits'] + 1;
                 $sql = "UPDATE users SET visits=".$visits." WHERE user_id=".$id;
                 mysqli_query($connect,$sql);
-                echo $visits;
+                echo "С возвращением! Это ваш".$visits."-й визит";
             } else {
                 $sql = "INSERT INTO users(school_title, report_grade, visits) VALUES ('".$_REQUEST['schoolTitle']."','".$_REQUEST['reportGrade']."', 1)";
                 $res = mysqli_query($connect,$sql);
-                echo 'Создан новый пользователь: '.$_REQUEST['reportGrade'];
+                echo "Здравствуйте! Похоже, вы у нас впервые";
             }
             mysqli_close($connect);
         }
